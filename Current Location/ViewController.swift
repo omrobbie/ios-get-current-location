3a2eb5980817f6fcf9f198d866b0d984c8a28def
omrobbie <putu.roby@yahoo.com>
2020-05-01 20:23:03 +0800

    Initial Commit

---
+++ Current Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Current Location
+//
+//  Created by omrobbie on 01/05/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
