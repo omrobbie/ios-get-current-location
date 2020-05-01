15e7b6bbf0192891bf7509e3125c187d8ef544df
omrobbie <putu.roby@yahoo.com>
2020-05-01 20:50:11 +0800

    get current location with geocoder

--- Current Location/ViewController.swift
+++ Current Location/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var lblResult: UILabel!
 
+    fileprivate let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLocationManager()
+    }
+
+    fileprivate func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.requestWhenInUseAuthorization() // You need to add NSLocationWhenInUseUsageDescription to Info.plist
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocation = manager.location else {return}
+        manager.stopUpdatingLocation()
+
+        CLGeocoder().reverseGeocodeLocation(location) { (placemark, error) in
+            if let error = error {
+                debugPrint("Error: \(error.localizedDescription)")
+                return
+            }
+
+            guard let placemark = placemark?.first else {return}
+            self.lblResult.text = "You are in \(placemark.administrativeArea ?? "somewhere"), \(placemark.country ?? "somewhere")"
+        }
     }
 }
